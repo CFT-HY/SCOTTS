412ba03decf5de60689cab696a0e03c5212cd695
David Weir <david.weir@helsinki.fi>
2012-07-26 20:13:12 +0300

    General tidy up

--- main.c
+++ main.c
@@ -5,10 +5,321 @@
 #include "hydro.h"
 
 
+int main(int argc, char *argv[])
+{
+
+  hydro_params p;
+
+#ifdef MPI
+
+  MPI_Init(&argc, &argv);
+  MPI_Comm_rank(MPI_COMM_WORLD, &p.rank);
+  MPI_Comm_size(MPI_COMM_WORLD, &p.size);
+
+  if(!p.rank)
+    fprintf(stderr, "3D hydro port with MPI\n");
+
+#else // MPI
+
+  fprintf(stderr, "3D hydro port (serial)\n");
+
+  p.rank = 0;
+  p.size = 1;
+
+#endif // MPI
+
+  if(argc != 2) {
+    if(!p.rank)
+      fprintf(stderr,"Usage: hydro <parameter file>\n");
+    return 100;
+  }
+
+
+#ifdef HAVE_MALLOC_H
+
+  // Inspired by Kari
+
+  mallopt( M_MMAP_MAX, 0 );  /* don't use mmap */
+  /* HACK: don't release memory by calling sbrk */
+  mallopt( M_TRIM_THRESHOLD, -1 );
+
+  fprintf(stderr, "Disabled sbrk\n");
+
+#endif // HAVE_MALLOC_H
+
+
+  // Parse params from stdin
+  get_parameters(argv[1], &p);
+
+  // How big is the system
+  p.N = p.Lx*p.Ly*p.Lz;
+
+  // Calculate terms in potential
+  p.alpha = 1.0/sqrt(2.0*p.sigma*
+			      pow(p.lcorr, 5.0)/3.0);
+
+  p.gamma = (p.Lheat 
+		      + 6.0*p.sigma/p.lcorr) 
+    / (6.0*p.sigma*p.lcorr);
+
+  p.lambda = 1.0/(3.0*p.sigma*pow(p.lcorr,3.0));
+
+  // What did we find?
+  if(!p.rank) 
+    fprintf(stderr,
+	    "-- calculated potential terms\n"	\
+	    "-- alpha %g, gamma %g, lambda %g\n",	\
+	    p.alpha, p.gamma, p.lambda);
+
+  // Make these user-modifiable eventually
+  p.Tconst = 0.86;
+  p.xxwall = -.5;
+
+  // gdeg = 34.25
+  p.a = 34.25*M_PI*M_PI/90.0;
+
+  // Ugly but it's a straight conversion of the fortran
+  p.T0 = sqrt(1.0-2.0*p.alpha*p.alpha
+		       /9.0/p.gamma/p.lambda);
+
+
+  // Set up layout for any (or no) parallelism
+  layout(&p);
+
+  // Useless
+  init_comms_time(&p);
+
+  // time iterate
+  int step;
+  double t = 0.0;
+
+  // space iterate
+  int x;
+
+  double wmax;
+
+  hydro_fields f;
+
+  double initial_energy, current_energy;
+
+  int **nb;
+  int **inverse;
+
+  double current_field_energy, current_wmax;
+
+  double cpu_time_used;
+
+  clock_t start, end;
+
+
+  nb = init_nb(&p);
+  inverse = init_inverse(&p);
+
+  if(!p.rank)
+    fprintf(stderr, "- Initialised neighbour (and inverse) lookup table.\n");
+
+
+
+  // Just runs malloc on all the fields therein
+  alloc_fields(&f, p);
+
+  if(!p.rank)
+    fprintf(stderr, "- Allocated fields.\n");
+
+
+  // For safety, set everything to zero
+  zero_fields(f, p);
+
+  if(!p.rank)
+    fprintf(stderr, "- Zeroed fields.\n");
+
+  // initial conditions
+  /*  if(p.initial == INIT_BUBBLE) {
+    create_gaussian_bubble(f, p);
+  } else if(p.initial == INIT_SHOCK_TUBE) {
+    create_shock_tube(f, p);
+  } else {
+    fprintf(stderr, "Unknown initial condition parameter!\n");
+    exit(100);
+  }
+  */
+
+  initial_scalar_bubble(f, p, inverse);
+  // initial_3D(f,p,inverse);
+  // initial_step(f,p);
+
+
+  // Communicate everything that neighbours need
+  halo_field(f.phi, p);
+  halo_field(f.pifull, p);
+  halo_field(f.E, p);
+  halo_field(f.Z[0], p);
+  halo_field(f.Z[1], p);
+  halo_field(f.Z[2], p);
+  halo_field(f.W, p);
+
+
+  if(!p.rank)
+    fprintf(stderr, "Initial conditions done\n");
+
+
+  initial_energy = reduce_sum(total_energy(f, nb, p), p);
+
+  if(!p.rank)
+    fprintf(stderr, "Initial avg energy per site: %g\n", 
+	    initial_energy);
+
+
+#ifdef SILO
+
+#ifdef MPI
+#error SILO and MPI do not play well yet
+#endif // MPI
+
+  if(p.silointerval > 0)
+    silo_init(p);
+
+#endif // SILO
+
+
+
+#ifdef PAPI
+
+  papi_init();
+
+#endif // PAPI
+
+
+
+  // Step back for leapfrog initial conds (doubtful we need this)
+  // evolve_backstep(f, nb, p);
+  // NB: if we later enable this, careful to halo the necessary stuff
+
+  cpu_time_used = 0.0;
+
+
+#ifdef MPI
+  // Mostly so that the timing is remotely fair
+  MPI_Barrier(MPI_COMM_WORLD);
+
+#endif // MPI
+
+
+  start = clock();
+
+  for(step = 0; step < p.steps; step++) {
+
+    if((p.silointerval > 0) && (step % p.silointerval == 0)) {
+
+#ifdef SILO
+
+      write_silo_step(f, p, step);
+
+#endif // SILO
+
+    }
+
+    if((p.interval > 0) && (step % p.interval == 0)) {
+
+      current_energy = reduce_sum(total_energy(f, nb, p), p);
+      current_field_energy = reduce_sum(field_energy(f, nb, p), p);
+      current_wmax = reduce_max(get_gamma_max(f, p), p);
+      
+      if(!p.rank)
+	fprintf(stderr,"%04d\t%6lf\t%6lf\t%6lf\t%6lf\n",
+		step, t,
+		current_energy,
+		current_field_energy,
+		current_wmax);
+      
+      /*
+      if(!p.rank)
+	fprintf(stderr, "Energy violation: %lf%%\n",
+		100.0*fabs((current_energy-initial_energy)/initial_energy));
+      */
+
+    }
+
+    // Do field step
+    evolve_field(f, nb, p);
+    
+    // Calculate EOS
+    eq_of_state(f, p);
+
+    // Do the hydro bits
+    evolve_hydro(f, nb, p);
+
+   
+    // Dump a whole lot of stuff (currently just use Silo for that)    
+    /*
+    if(step == p.steps - 1)
+      for(x=0;x<p.N;x++) {
+	if(inverse[x][0] == inverse[x][1]) {
+	  fprintf(stdout,"%lf %.10lf %.10lf %.10lf %.10lf %.10lf\n",
+		  (inverse[x][0]*p.dx),
+		  f.V[0][x],
+		  f.U[0][x],
+		  f.phi[x],
+		  f.T[x],
+		  f.Z[0][x]);
+	  
+	}
+      }    
+    */
+
+    // Advection of state variables
+    advect_E(f, nb, p);
+    // Advection of momentum
+    advect_Z(f, nb, p);
+
+    // Don't bother with art viscosity, yet
+    // artificial_viscosity(f, nb, p);
+
+    // Solve for T
+    find_Ta(f, p);
+    
+    t += p.dt;
+    
+  } // main loop ends here
+
+
+  end = clock();
+
+
+#ifdef PAPI
+
+  papi_finalise();
+
+#endif // PAPI
+  
+
+#ifdef MPI
+
+  MPI_Finalize();
+
+#endif // MPI
+
+
+  cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+
+  fprintf(stderr, "CPU time in main loop was %lf, of which %lf was comms\n",
+	  cpu_time_used, get_comms_time(&p));
+
+
+  // Clean up memory
+  free_nb(nb, &p);
+  free_inverse(inverse, &p);
+  free_fields(&f);
+  
+  return 0;
+
+}
+
+
+
+
+
 void alloc_fields(hydro_fields *f, hydro_params p) {
-  // location labels set up in initial condition functions
-  f->xe = (double *) malloc(p.fieldN*sizeof(double));
-  f->xc = (double *) malloc(p.fieldN*sizeof(double));
 
   // fields below also set up in initial condition functions
   f->phi = (double *) malloc(p.fieldN*sizeof(double));
@@ -61,15 +372,7 @@ void alloc_fields(hydro_fields *f, hydro_params p) {
 }
 
 
-void dump(double *field, hydro_params p) {
-  int x;
 
-  fprintf(stderr,"%g", field[0]);
-  for(x=1;x<p.N;x++) {
-    fprintf(stderr,", %g", field[x]);
-  }
-  fprintf(stderr,"\n");
-}
 
 void zero_fields(hydro_fields f, hydro_params p) {
 
@@ -78,8 +381,6 @@ void zero_fields(hydro_fields f, hydro_params p) {
   // We don't need to do this because create_gaussian_bubble
   // initialises everything, but it keeps valgrind quiet.
   for(x=0;x<p.fieldN;x++) {
-    f.xe[x] = 0.0000;
-    f.xc[x] = 0.0000;
 
     f.phi[x] = 0.0000;
     f.pifull[x] = 0.0000;
@@ -105,8 +406,6 @@ void zero_fields(hydro_fields f, hydro_params p) {
 }
 
 void free_fields(hydro_fields *f) {
-  free(f->xe);
-  free(f->xc);
 
   free(f->phi);
   free(f->pifull);
@@ -150,291 +449,3 @@ void free_fields(hydro_fields *f) {
  
 }
 
-
-
-int main(int argc, char *argv[])
-{
-
-
-  hydro_params p;
-
-#ifdef MPI
-  MPI_Init(&argc, &argv);
-  MPI_Comm_rank(MPI_COMM_WORLD, &p.rank);
-  MPI_Comm_size(MPI_COMM_WORLD, &p.size);
-#else // MPI
-  p.rank = 0;
-  p.size = 1;
-#endif // MPI
-
-
-  if(!p.rank)
-    fprintf(stderr,"3D hydro port\n");
-
-  if(argc != 2) {
-    if(!p.rank)
-      fprintf(stderr,"Usage: hydro <parameter file>\n");
-    return 100;
-  }
-
-#ifdef HAVE_MALLOC_H
-  mallopt( M_MMAP_MAX, 0 );  /* don't use mmap */
-  /* HACK: don't release memory by calling sbrk */
-  mallopt( M_TRIM_THRESHOLD, -1 );
-  fprintf(stderr, "Disabled sbrk\n");
-#endif // HAVE_MALLOC_H
-
-
-  // Parse params from stdin
-  get_parameters(argv[1],&p);
-
-
-
-  p.N = p.Lx*p.Ly*p.Lz;
-
-  // Calculate terms in potential
-  p.alpha = 1.0/sqrt(2.0*p.sigma*
-			      pow(p.lcorr, 5.0)/3.0);
-
-  p.gamma = (p.Lheat 
-		      + 6.0*p.sigma/p.lcorr) 
-    / (6.0*p.sigma*p.lcorr);
-
-  p.lambda = 1.0/(3.0*p.sigma*pow(p.lcorr,3.0));
-
-  // What did we find?
-  if(!p.rank) 
-    fprintf(stderr,
-	    "-- calculated potential terms\n"	\
-	    "-- alpha %g, gamma %g, lambda %g\n",	\
-	    p.alpha, p.gamma, p.lambda);
-
-  // Make these user-modifiable eventually
-  p.Tconst = 0.86;
-  p.xxwall = -.5;
-
-  // gdeg = 34.25
-  p.a = 34.25*M_PI*M_PI/90.0;
-
-  // Ugly but it's a straight conversion of the fortran
-  p.T0 = sqrt(1.0-2.0*p.alpha*p.alpha
-		       /9.0/p.gamma/p.lambda);
-
-
-
-
-  // Set up layout for any parallelism
-  layout(&p);
-
-  // Useless
-  init_comms_time(&p);
-
-  // time iterate
-  int step;
-  double t = 0.0;
-
-  // space iterate
-  int x;
-
-  double wmax;
-
-  hydro_fields f;
-
-  double initial_energy, current_energy;
-
-  int **nb;
-
-  nb = init_nb(&p);
-
-  if(!p.rank)
-    fprintf(stderr, "- Initialised neighbour lookup table.\n");
-
-
-  //  fprintf(stderr,"Check get_x(0,p): %d\n", get_x(0,p));
-
-  // Just runs malloc on all the fields therein
-  alloc_fields(&f, p);
-
-  if(!p.rank)
-    fprintf(stderr, "- Allocated fields.\n");
-
-  // For safety, set everything to zero
-  zero_fields(f, p);
-
-  if(!p.rank)
-    fprintf(stderr, "- Zeroed fields.\n");
-
-  // initial conditions
-  /*  if(p.initial == INIT_BUBBLE) {
-    create_gaussian_bubble(f, p);
-  } else if(p.initial == INIT_SHOCK_TUBE) {
-    create_shock_tube(f, p);
-  } else {
-    fprintf(stderr, "Unknown initial condition parameter!\n");
-    exit(100);
-  }
-  */
-
-  initial_scalar_bubble(f,p);
-  //      initial_3D(f,p);
-    // initial_step(f,p);
-
-  halo_field(f.phi, p);
-  halo_field(f.pifull, p);
-  halo_field(f.E, p);
-  halo_field(f.Z[0], p);
-  halo_field(f.Z[1], p);
-  halo_field(f.Z[2], p);
-  halo_field(f.W, p);
-
-
-  if(!p.rank)
-    fprintf(stderr, "Initial conditions done\n");
-
-
-  initial_energy = reduce_sum(total_energy(f, nb, p),p);
-
-  if(!p.rank)
-    fprintf(stderr, "Initial avg energy per site: %g\n", 
-	    initial_energy);
-
-  // Output headers
-  // printf("Step\ttime\tenergy\twallps\n");
-
-  // Output of some field at every time step
-  //  FILE *phi_fh = fopen("field.dat","w");
-#ifdef SILO
-  if(p.silointerval > 0)
-    silo_init(p);
-#endif // SILO
-
-#ifdef PAPI
-  papi_init();
-#endif // PAPI
-
-  // Step back for leapfrog initial conds
-  //  evolve_backstep(f, nb, p);
-
-  // Record how many lattice sites in output file, to keep self contained
-  //  fwrite(&p.N, sizeof(int), 1, phi_fh);
-
-
-  double current_field_energy, current_wmax;
-
-  double cpu_time_used = 0.0;
-  clock_t start, end;
-
-#ifdef MPI
-  MPI_Barrier(MPI_COMM_WORLD);
-#endif // MPI
-
-  start = clock();
-
-  for(step = 0; step < p.steps; step++) {
-
-    if((p.silointerval > 0) && (step % p.silointerval == 0)) {
-#ifdef SILO
-      write_silo_step(f, p, step);
-#endif // SILO
-    }
-
-    if((p.interval > 0) && (step % p.interval == 0)) {
-
-
-      // Write time step, x coords and velocity field
-      //      fwrite(&t, sizeof(double), 1, phi_fh);
-      //      fwrite(f.E, sizeof(double), p.N, phi_fh);
-      //      fwrite(f.p, sizeof(double), p.N, phi_fh);
-
-      current_energy = reduce_sum(total_energy(f, nb, p),p);
-      current_field_energy = reduce_sum(field_energy(f, nb, p),p);
-      current_wmax = reduce_max(wmax, p);
-      
-      if(!p.rank)
-	fprintf(stderr,"%04d\t%6lf\t%6lf\t%6lf\t%6lf\n",
-		step, t,
-		current_energy,
-		current_field_energy,
-		current_wmax);
-      
-
-      fflush(stdout);
-
-      if(!p.rank)
-	fprintf(stderr, "Energy violation: %lf%%\n",
-		100.0*fabs((current_energy-initial_energy)/initial_energy));
-    }
-
-    // Do field step
-    evolve_field(f, nb, p);
-
-    
-    
-    // Calculate EOS
-    eq_of_state(f, p);
-
-
-
-    // Do the hydro bits
-    evolve_hydro(f, nb, p);
-
-    
-    if(step == p.steps - 1)
-      for(x=0;x<p.N;x++) {
-	if(get_x(x,p) == get_y(x,p)) {
-	  fprintf(stdout,"%lf %.10lf %.10lf %.10lf %.10lf %.10lf\n",
-		  (get_x(x,p)*p.dx),
-		  f.V[0][x],
-		  f.U[0][x],
-		  f.phi[x],
-		  f.T[x],
-		  f.Z[0][x]);
-	}
-      }    
-
-    // Advection of state variables
-    advect_E(f, nb, p);
-    // Advection of momentum
-    advect_Z(f, nb, p);
-
-    //    artificial_viscosity(f, nb, p);
-
-
-
-    // Solve for T
-    find_Ta(f, p);
-
-
-    
-    t += p.dt;
-
-    wmax = get_gamma_max(f, p);
-
-
-    
-  } // main loop ends here
-
-  end = clock();
-
-#ifdef PAPI
-  papi_finalise();
-#endif // PAPI
-  
-
-#ifdef MPI
-  MPI_Finalize();
-#endif // MPI
-
-
-  cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
-
-  fprintf(stderr,"CPU time in main loop was %lf, of which %lf was comms\n", cpu_time_used, get_comms_time(&p));
-
-  //  fclose(phi_fh);
-
-  // Clean up memory
-  free_nb(nb, &p);
-  free_fields(&f);
-  
-  return 0;
-}
