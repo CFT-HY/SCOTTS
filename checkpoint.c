bef9ab08d3f02bc3605f7ab622b5e7e703b78724
David Weir <david.weir@helsinki.fi>
2012-10-10 12:14:57 +0300

    Used die and printf0 in checkpoint.c after merge

--- checkpoint.c
+++ checkpoint.c
@@ -15,110 +15,109 @@ int load_checkpoint(hydro_fields f, hydro_params p)
   
   if(fread(&sizex, sizeof(int), 1, fp) != 1) {
     fprintf(stderr, "Error reading sizex\n");
-    exit(100);
+    die(100);
   }
 
 
   if(fread(&sizey, sizeof(int), 1, fp) != 1) {
     fprintf(stderr, "Error reading sizey\n");
-    exit(100);
+    die(100);
   }
 
   if(fread(&sizez, sizeof(int), 1, fp) != 1) {
     fprintf(stderr, "Error reading sizez\n");
-    exit(100);
+    die(100);
   }
 
 
   if(fread(&step, sizeof(int), 1, fp) != 1) {
     fprintf(stderr, "Error reading step\n");
-    exit(100);
+    die(100);
   }
 
 
   if(fread(f.phi[0][0], sizeof(double), sizex*sizey*sizez, fp) != sizex*sizey*sizez) {
     fprintf(stderr, "Error reading phi\n");
-    exit(100);
+    die(100);
   }
 
   if(fread(f.pi[0][0], sizeof(double), sizex*sizey*sizez, fp) != sizex*sizey*sizez) {
     fprintf(stderr, "Error reading pi\n");
-    exit(100);
+    die(100);
   }
 
   if(fread(f.pifull[0][0], sizeof(double), sizex*sizey*sizez, fp) != sizex*sizey*sizez) {
     fprintf(stderr, "Error reading pifull\n");
-    exit(100);
+    die(100);
   }
 
   if(fread(f.T[0][0], sizeof(double), sizex*sizey*sizez, fp) != sizex*sizey*sizez) {
     fprintf(stderr, "Error reading T\n");
-    exit(100);
+    die(100);
   }
 
   if(fread(f.kappa[0][0], sizeof(double), sizex*sizey*sizez, fp) != sizex*sizey*sizez) {
     fprintf(stderr, "Error reading kappa\n");
-    exit(100);
+    die(100);
   }
 
   if(fread(f.phiold[0][0], sizeof(double), sizex*sizey*sizez, fp) != sizex*sizey*sizez) {
     fprintf(stderr, "Error reading phiold\n");
-    exit(100);
+    die(100);
   }
 
   if(fread(f.p[0][0], sizeof(double), sizex*sizey*sizez, fp) != sizex*sizey*sizez) {
     fprintf(stderr, "Error reading p\n");
-    exit(100);
+    die(100);
   }
 
   if(fread(f.E[0][0], sizeof(double), sizex*sizey*sizez, fp) != sizex*sizey*sizez) {
     fprintf(stderr, "Error reading E\n");
-    exit(100);
+    die(100);
   }
 
   if(fread(f.W[0][0], sizeof(double), sizex*sizey*sizez, fp) != sizex*sizey*sizez) {
     fprintf(stderr, "Error reading W\n");
-    exit(100);
+    die(100);
   }
 
   if(fread(f.V[0][0][0], sizeof(double), sizex*sizey*sizez*3, fp) != sizex*sizey*sizez*3) {
     fprintf(stderr, "Error reading V\n");
-    exit(100);
+    die(100);
   }
 
   if(fread(f.U[0][0][0], sizeof(double), sizex*sizey*sizez*3, fp) != sizex*sizey*sizez*3) {
     fprintf(stderr, "Error reading U\n");
-    exit(100);
+    die(100);
   }
 
   if(fread(f.F[0][0][0], sizeof(double), sizex*sizey*sizez*3, fp) != sizex*sizey*sizez*3) {
     fprintf(stderr, "Error reading F\n");
-    exit(100);
+    die(100);
   }
 
   if(fread(f.Z[0][0][0], sizeof(double), sizex*sizey*sizez*3, fp) != sizex*sizey*sizez*3) {
     fprintf(stderr, "Error reading Z\n");
-    exit(100);
+    die(100);
   }
 
   char endmatter[6];
   if(fread(endmatter, sizeof(char), 5, fp) != 5) {
     fprintf(stderr, "Did not read endmatter\n");
-    exit(100);
+    die(100);
   } else {
     if(strncmp("ORDYH",endmatter,5) != 0) {
       fprintf(stderr, "Endmatter incorrect\n");
-      exit(100);
+      die(100);
     }
   }
   if(fgetc(fp) != EOF) {
     fprintf(stderr, "Not reached eof after endmatter\n");
-    exit(100);    
+    die(100);    
   }
 
 
-  if(!p.rank)
-    fprintf(stderr, "Done loading checkpoint, at step %d\n", step);
+  printf0(p, "Done loading checkpoint, at step %d\n", step);
 
   return step;
 }
@@ -131,9 +130,8 @@ int usable_checkpoint(hydro_fields f, hydro_params p)
 
   int happy = 1;
 
-  if(!p.rank) {
-    fprintf(stderr, "Checking for valid checkpoint\n");
-  }
+  printf0(p, "Checking for valid checkpoint\n");
+
 
   sprintf(filename, "%s/checkpoint-integrity", p.checkpointdir);
 
@@ -177,9 +175,8 @@ int usable_checkpoint(hydro_fields f, hydro_params p)
 	  fprintf(stderr, "Sizes do not match (%d vs %d), (%d vs %d), (%d vs %d)\n", sizex, p.slicex+2, sizey, p.slicey+2, sizez, p.Lz);
 	  happy = 0;
 	} else {
-	  if(!p.rank) {
-	    fprintf(stderr, "Looks like we can restart to step %d\n", step);
-	  }
+	  printf0(p, "Looks like we can restart to step %d\n", step);
+
 	  
 	  fseek(fp, 21*sizex*sizey*sizez*sizeof(double), SEEK_CUR);
 	  char endmatter[6];
@@ -239,8 +236,7 @@ void checkpoint(hydro_fields f, hydro_params p, int step)
 
   sprintf(filename, "%s/checkpoint-%06d", p.checkpointdir, p.rank);  
 
-  if(!p.rank)
-    fprintf(stderr,"Checkpointing in %s\n",p.checkpointdir);
+  printf0(p,"Checkpointing in %s\n",p.checkpointdir);
 
 
   FILE *fp = fopen(filename, "w");
@@ -295,8 +291,7 @@ void checkpoint(hydro_fields f, hydro_params p, int step)
 
   double end = clock();
 
-  if(!p.rank)
-    fprintf(stderr,"checkpointing took %lf\n",
+  printf0(p, "checkpointing took %lf\n",
             ((double) (end - start)) / CLOCKS_PER_SEC);
 
 
