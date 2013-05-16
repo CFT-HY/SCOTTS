378bd32b9d4bfbb85b1b210ee399b14ee77fcb51
David Weir <david.weir@helsinki.fi>
2013-05-16 21:31:59 +0300

    Tidied up potential.c

--- potential.c
+++ potential.c
@@ -5,7 +5,10 @@
 #include "hydro.h"
 
 
-// Potential
+/* double Vf(hydro_params p, double T, double this_phi)
+ *
+ * Potential.
+ */
 double Vf(hydro_params p, double T, double this_phi) {
   return  0.5*p.gamma*(T*T - p.T0*p.T0)*this_phi*this_phi
     - p.alpha*T*this_phi*this_phi*this_phi/3.0
@@ -13,7 +16,10 @@ double Vf(hydro_params p, double T, double this_phi) {
 }
 
 
-// First derivative wrt phi
+/* double Vdf(hydro_params p, double T, double this_phi)
+ *
+ * First derivative wrt phi
+ */
 double Vdf(hydro_params p, double T, double this_phi) {
   return p.gamma*(T*T - p.T0*p.T0)*this_phi
     - p.alpha*T*this_phi*this_phi
@@ -21,20 +27,26 @@ double Vdf(hydro_params p, double T, double this_phi) {
 }
 
 
-// First derivative wrt T
+/* double VTf(hydro_params p, double T, double this_phi)
+ *
+ * First derivative wrt T
+ */
 double VTf(hydro_params p, double T, double this_phi) {
   return p.gamma*T*this_phi*this_phi 
     - p.alpha*this_phi*this_phi*this_phi/3.0;
 }
 
 
-// Second derivative wrt T
+/* double VTTf(hydro_params p, double T, double this_phi)
+ *
+ * Second derivative wrt T
+ */
 double VTTf(hydro_params p, double T, double this_phi) {
   return p.gamma*this_phi*this_phi;
 }
 
 
-/*  Vpot(...)
+/* void Vpot(hydro_params p, double *T, double *phi, double *Vprecalc)
  *
  * Calculate potential for all sites. Except that doing it this way
  * does not encourage the compiler to fuse the loops.
@@ -50,7 +62,7 @@ void Vpot(hydro_params p,
 }
 
 
-/* Vdpot(...)
+/* void Vdpot(hydro_params p, double *T, double *phi, double *Vprecalc)
  *
  * Calculate first deriviative of potential wrt phi for all sites.
  */
