d94a696aee9ad1bc05da67d2b8671818f19228c2
David Weir <david.weir@helsinki.fi>
2013-05-17 13:08:38 +0300

    Tidied up main.c

--- main.c
+++ main.c
@@ -18,7 +18,7 @@ int main(int argc, char *argv[])
 
   printf0(p, "3D hydro port with MPI\n");
 
-#else // MPI
+#else // not MPI
 
   printf0(p, "3D hydro port (serial)\n");
 
@@ -27,6 +27,7 @@ int main(int argc, char *argv[])
 
 #endif // MPI
 
+  // Note that this is when main.c was rebuilt, not the whole thing!
   printf0(p, "Built: %s %s\n", __DATE__, __TIME__);
 
   if(argc != 2) {
@@ -61,69 +62,69 @@ int main(int argc, char *argv[])
 
   //  p.scale = 0.3;
 
-  // Calculate terms in potential
+  // Calculate potential terms, no longer used (potential directly supplied)
   /*
   p.alpha = 1.0/sqrt(2.0*p.sigma*pow(p.lcorr, 5.0)/3.0);
-
   p.gamma = (p.Lheat + 6.0*p.sigma/p.lcorr)/(6.0*p.sigma*p.lcorr);
-
   p.lambda = 1.0/(3.0*p.sigma*pow(p.lcorr,3.0));
-  */
 
-  // What did we find?
   printf0(p,
 	  "-- calculated potential terms\n"
 	  "-- alpha %g, gamma %g, lambda %g\n",
 	  p.alpha, p.gamma, p.lambda);
 
-
-  // Make these user-modifiable eventually
-  //  p.Tconst = 0.86;
-
-  // gdeg = 34.25
-  p.a = 34.25*M_PI*M_PI/90.0;
-
-  // Ugly but it's a straight conversion of the fortran
-  // p.T0 = sqrt(1.0-2.0*p.alpha*p.alpha/9.0/p.gamma/p.lambda);
+  p.T0 = sqrt(1.0-2.0*p.alpha*p.alpha/9.0/p.gamma/p.lambda);
 
   printf0(p,
 	  "-- T0 %g\n", p.T0);
+  */
+
+
+  // Degrees of freedom, still hardcoded
+  p.a = 34.25*M_PI*M_PI/90.0;
+
 
   // Set up layout for any (or no) parallelism
   layout(&p);
 
-
   // Start a counter for communications time
   init_comms_time(&p);
 
-
+  // Initialised bubble count
   int bcount = 0;
 
-  // time iterates: count steps and t
+  // On this timestep, do we continue to nucleate?
+  int still_nucleate = 1;
+
+  // How many bubbles to nucleate on a given timestep
+  int howmany;
+
+  // time iterates: count steps and t separately
   int step;
   double t = 0.0;
 
-  // space iterate
-  int x;
-
-  double wmax;
-  double cpts[TENSOR_CPTS];
-
+  // Struct that stores the fields
   hydro_fields f;
 
+  // Storage of measurements of average stress-energy tensor (not used)
+  //  double cpts[TENSOR_CPTS];
+
   double initial_energy, current_energy;
+  double initial_field_energy, current_field_energy;
+  double current_kinetic, current_gradient_energy, current_rest;
+  double wmax, current_wmax;
+  double gwen;
 
-  double initial_field_energy, current_field_energy, current_wmax, current_kinetic, current_gradient_energy, current_rest;
-
+  // Timing counters
   double cpu_time_used;
 
   clock_t start, end;
 
-  int still_nucleate = 1;
-
+  // What step does the for loop start on? For checkpoint restarts...
   int step_start = 0;
 
-  double gwen;
+
+  int i;
 
   // Just runs malloc on all the fields therein (see alloc.c)
   alloc_fields(&f, p);
@@ -131,6 +132,8 @@ int main(int argc, char *argv[])
   // Managed to get this far, so we probably have enough memory
   printf0(p, "- Allocated fields.\n");
 
+
+  // Restore checkpoint
   if(usable_checkpoint(f, p)) {
     printf0(p, "Found a usable checkpoint file\n");
 
@@ -139,31 +142,24 @@ int main(int argc, char *argv[])
     printf0(p, "Checkpoint load done.\n");
     printf0(p, "WARNING: Bubble count reset after restart!\n");
 
-    // bodge: reseed badly
-    //    srandom(abs(random() - step_start));
-    
   } else {
     // For safety, set everything to zero
     zero_fields(f, p);
 
     printf0(p, "- Zeroed fields.\n");
 
-    // initial conditions -- one or more bubbles, or blank and then nucleate?
-    /*  if(p.initial == INIT_BUBBLE) {
-	create_gaussian_bubble(f, p);
-	} else if(p.initial == INIT_SHOCK_TUBE) {
-	create_shock_tube(f, p);
-	} else {
-	fprintf(stderr, "Unknown initial condition parameter!\n");
-	die(100);
-	}
-    */
 
+    // One bubble only (not used)
+    // initial_scalar_bubble(f, p);
+
+    // Shock tube style initial conditions for fluid only (not used)
+    // initial_3D(f,p);
+
+
+    // Instead, start off with an empty box
     initial_blank(f, p);
 
-    // You get one bubble for free
-    //  initial_scalar_bubble(f, p);
-
+    // p.bubbles is how many bubbles to make at the start (usually 1)
     for(step=0;step<p.bubbles;step++) {
       start = clock();
       still_nucleate = do_nucleate(f, p);
@@ -180,10 +176,8 @@ int main(int argc, char *argv[])
 
     }
 
+    // (don't) turn off nucleation after initial stage
     //  still_nucleate = 0;
-    // initial_3D(f,p);
-    // initial_step(f,p);
-
 
 
 
@@ -206,11 +200,12 @@ int main(int argc, char *argv[])
   printf0(p, "Initial avg energy per site: %g\n", 
 	  initial_energy/((double)p.N));
 
+  // Set up unequal time correlator stuff (not used)
   //  init_uetc(f, p);
-
 
 #ifdef SILO
 
+  // Are we using silo for visualisation?
   if(p.silointerval > 0)
     silo_init(p);
 
@@ -220,17 +215,18 @@ int main(int argc, char *argv[])
 
 #ifdef PAPI
 
+  // Are we using PAPI performance counters?
   papi_init();
 
 #endif // PAPI
 
-
+  // Reset cpu time
+  cpu_time_used = 0.0;
 
   // Step back for leapfrog initial conds (doubtful we need this)
   // evolve_backstep(f, p);
   // NB: if we later enable this, careful to halo the necessary stuff
 
-  cpu_time_used = 0.0;
 
 
 #ifdef MPI
@@ -240,40 +236,44 @@ int main(int argc, char *argv[])
 
 #endif // MPI
 
+  // For making sound files of the pressure
+  /*
   FILE *press;
 
 
   if(!p.rank) {
     press = fopen("pressure.dat","a");
   }
+  */
 
   printf0(p, "Starting main loop.\n");
 
-
+  // Wall time measurement
   start = clock();
 
-  double t00;
-  int howmany, i;
+
 
   for(step = step_start; step < p.steps; step++) {
 
+    /*
     if(!p.rank) {
       fprintf(press, "%d %g\n", step, f.p[1][1][1]);
     }
+    */
 
-    // How about this for reproducibility?
+
+
+    // For reproducibility, reseed with timestep
+    // so bubbles nucleate in same places (locations given by random())
     srandom(p.seed + step);
 
-
-    // Disabled: always try!
-    //     if(still_nucleate && should_nucleate(f, p, t, step)) {
-
-    
-
+    // How many bubbles do we (try to) nucleate this timestep?
     howmany = should_nucleate(f, p, t, step);
+
     i = 0;
 
     while(i < howmany) {
+
       still_nucleate = do_nucleate(f, p);
 
       bcount += still_nucleate;
@@ -281,38 +281,43 @@ int main(int argc, char *argv[])
     }
 
 
+    // Checkpoint if necessary
     if((p.checkpointinterval > 0) && (step % p.checkpointinterval == 0) \
        && (step != step_start)) {
       int wall_start = (int)time(NULL);
       checkpoint(f, p, step);
-      printf0(p, "Checkpointing took %d seconds of walltime\n", ((int)time(NULL))-wall_start);
+      printf0(p, "Checkpointing took %d seconds of walltime\n",
+	      ((int)time(NULL))-wall_start);
     }
 
-
-    if((p.silointerval > 0) && (step % p.silointerval == 0)) {
 
 #ifdef SILO
-
+    // Write visualisation stuff if necessary
+    if((p.silointerval > 0) && (step % p.silointerval == 0)) {
       write_silo_step(f, p, step);
-
+    }
 #endif // SILO
 
-    }
 
+    // Measurements
     if((p.interval > 0) && (step % p.interval == 0)) {
-
-
 
 
 #ifdef FFT
       histo_field(f.phi, p, step);
 
-      //      fft_uetc(f, p, step);
+      // Calculate UETCs (not in use)
+      // fft_uetc(f, p, step);
       
+      // Power spectrum of scalar field
       fft_field(f, p, f.phi, step);
+      // Velocity power spectrum
       fft_vel(f, p, step);
+      // Gravitational wave power spectrum (returns GW energy)
       gwen = fft_tensor(f, p, step, current_energy);
 
+      // Average size of stress-energy tensor components (not used)
+      /*
       didj(cpts, f, p);
       printf0(p, "cpts: %g %g %g %g %g %g\n",
 	      cpts[CPT_11],
@@ -320,7 +325,8 @@ int main(int argc, char *argv[])
 	      cpts[CPT_31],
 	      cpts[CPT_22],
 	      cpts[CPT_32],
-	      cpts[CPT_33]);    
+	      cpts[CPT_33]);
+      */
 #endif // FFT
 
 
@@ -330,7 +336,8 @@ int main(int argc, char *argv[])
       current_rest = reduce_sum(rest_energy(f, p), p);
       current_field_energy = reduce_sum(field_energy(f, p), p);
       current_gradient_energy = reduce_sum(gradient_energy(f, p), p);
-      current_wmax = reduce_sum(get_veltot(f, p), p)/((double)(p.Lx*p.Ly*p.Lz));
+      current_wmax = reduce_sum(get_veltot(f, p), p)
+	/((double)(p.Lx*p.Ly*p.Lz));
       
       if(!p.rank) {
 	printf("%04d\t%6lf\t%6lf\t%6lf\t%6lf\t%6lf\t%6lf\t%6lf\t%d\t%6lf\n",
@@ -346,10 +353,13 @@ int main(int argc, char *argv[])
 	       current_rest);
       }
 
+      // Statement of energy violation (not shown; better to use KE)
       /*
       fprintf(stderr, "Energy violation: %.10lf%%, %lf%%\n",
-              100.0*fabs((current_energy-initial_energy)/initial_energy),
-              100.0*fabs((current_field_energy-initial_field_energy)/initial_field_energy));
+              100.0*fabs((current_energy-initial_energy)
+			 /initial_energy),
+              100.0*fabs((current_field_energy-initial_field_energy)
+			 /initial_field_energy));
       */
     }
 
@@ -357,14 +367,12 @@ int main(int argc, char *argv[])
     // Do field step
     evolve_field(f, p);
 
-
     // Calculate EOS
     eq_of_state(f, p);
 
     // Do the hydro bits
     evolve_hydro(f, p);
 
-    //    double t0 = 320.0;
 #ifdef CUTOFF
     double cutoff = 0.5*(1.0-tanh(10.0*(t/p.tcutoff - 0.9)));
 
@@ -374,37 +382,9 @@ int main(int argc, char *argv[])
 
     // Evolve metric perturbations
     evolve_uij(f, p, cutoff);
-#else
+#else // not cutoff
     evolve_uij(f, p);
 #endif
-
-    // Dump a whole lot of stuff (currently just use Silo for that)    
-    if(step == p.steps - 1) {
-
-#ifdef FFT
-      
-      fft_tensor(f,p,step,current_energy);
-    
-#endif // FFT
-    
-
-    /*
-      for(x=0;x<p.N;x++) {
-	if(inverse[x][0] == inverse[x][1]) {
-	  fprintf(stdout,"%lf %.10lf %.10lf %.10lf %.10lf %.10lf\n",
-		  (inverse[x][0]*p.dx),
-		  f.V[0][x],
-		  f.U[0][x],
-		  f.phi[x],
-		  f.T[x],
-		  f.Z[0][x]);
-	  
-	}
-      }    
-
-    */
-
-    }
 
     // Advection of state variables
     advect_E(f, p);
@@ -418,33 +398,49 @@ int main(int argc, char *argv[])
     find_Ta(f, p);
     
     t += p.dt;
+
+
+    // On the last step, do some extra GW power spectra FFTs
+#ifdef FFT
+    if(step == p.steps - 1) {
+      fft_tensor(f,p,step,current_energy);
+    }
+#endif // FFT
     
   } // main loop ends here
+
 
   if(p.checkpointinterval > 0) {
     printf0(p, "Final checkpoint...\n");
     checkpoint(f, p, step);
   }
-  
+
+
   current_energy = reduce_sum(total_energy(f, p), p);
   current_kinetic = reduce_sum(kinetic_energy(f, p), p);
+  current_rest = reduce_sum(rest_energy(f, p), p);
   current_field_energy = reduce_sum(field_energy(f, p), p);
-  current_wmax = reduce_max(get_gamma_max(f, p), p);
-  
-  //  printf0(p, "Final state:\n");
+  current_gradient_energy = reduce_sum(gradient_energy(f, p), p);
+  current_wmax = reduce_sum(get_veltot(f, p), p)
+    /((double)(p.Lx*p.Ly*p.Lz));
+      
   if(!p.rank) {
-    printf("%04d\t%6lf\t%6.10lf\t%6lf\t%6lf\t%6lf\t%d\n",
+    printf("%04d\t%6lf\t%6lf\t%6lf\t%6lf\t%6lf\t%6lf\t%6lf\t%d\t%6lf\n",
 	   step,
 	   t,
 	   current_energy,
 	   current_kinetic,
 	   current_field_energy,
+	   current_gradient_energy,
 	   current_wmax,
-	   bcount);
+	   gwen,
+	   bcount,
+	   current_rest);
   }
 
+  
+  // End time, for walltime calculation
   end = clock();
-
 
 
 #ifdef FFT
@@ -469,6 +465,7 @@ int main(int argc, char *argv[])
 #endif // MPI
 
 
+  // Time spent running
   cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
 
   printf0(p, "On master node, CPU time in main loop was %lfs,\n"
