cd2917af7660acb17c39520d26e3d303cf088036
David Weir <weir@taito-login4.csc.fi>
2014-05-09 19:29:40 +0300

    Added taito jobscript

---
+++ run/rerun-0.4/jobtaito.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -l
+#SBATCH -J rerun-0.4
+#SBATCH -e rerun-0.4_err_%j
+#SBATCH -o rerun-0.4_out_%j
+#SBATCH --mem-per-cpu=2000
+#SBATCH -t 0:30:00
+#SBATCH -n 16
+#SBATCH -p test
+
+module load intel
+
+mkdir -p /wrk/weir/rerun-0.4/
+
+srun ~/appl_taito/projects/newhydro/run/hydro input-intermediate-0.4-both
