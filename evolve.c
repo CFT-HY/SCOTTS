0bc79b8ae220ddc2910c7b8eea5193c1f9aec92a
David Weir <david.weir@helsinki.fi>
2018-03-12 14:06:42 +0000

    evolve.c edited online with Bitbucket

--- evolve.c
+++ evolve.c
@@ -46,19 +46,19 @@ void evolve_field(hydro_fields f, hydro_params p) {
 	 * Gradients and velocities live on cell boundaries,
 	 * but we want the values in the body of the cell so we average the two boundaries.
 	 */ 
-	f.pi[x][y][z] = f.pi[x][y][z] +s*(
-					
-					  (f.V[0][x][y][z]*(f.phi[x][y][z]
+	f.pi[x][y][z] = f.pi[x][y][z] -p.dt*(p.C*f.phi[x][y][z]*f.phi[x][y][z]
+                                         /f.T[x][y][z])*f.W[x][y][z]*(
+					   0.5*(f.V[0][x][y][z]*(f.phi[x][y][z]
 							    - f.phi[x-1][y][z])
 					   + f.V[0][x+1][y][z]*(f.phi[x+1][y][z]
 								- f.phi[x][y][z]))/p.dx
 					 
-					  +(f.V[1][x][y][z]*(f.phi[x][y][z]
+					  + 0.5*(f.V[1][x][y][z]*(f.phi[x][y][z]
 							     - f.phi[x][y-1][z])
 					    + f.V[1][x][y+1][z]*(f.phi[x][y+1][z]
 								 - f.phi[x][y][z]))/p.dx
 					  
-					  +(f.V[2][x][y][z]*(f.phi[x][y][z]
+					  + 0.5*(f.V[2][x][y][z]*(f.phi[x][y][z]
 							     - f.phi[x][y][((z-1+p.Lz)%p.Lz)])
 					    + f.V[2][x][y][((z+1)%p.Lz)]*(f.phi[x][y][((z+1)%p.Lz)]
 									  - f.phi[x][y][z]))/p.dx	
