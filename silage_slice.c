c66ea67238d5afa0513559fbc4e8eaeee822985f
Pierre Auclair <pierre.auclair@iap.fr>
2019-02-11 11:41:45 +0100

    Enabled vectors in silage_slice, corrected merge conflicts, better initps input file

--- silage_slice.c
+++ silage_slice.c
@@ -2,9 +2,9 @@
  *
  * Module to write slices of silo data for visualisations.
  *
- * Slicing is done through the x coordinate at `x=p.siloslicecoord` 
+ * Slicing is done through the x coordinate at `x=p.siloslicecoord`
  * where `p.siloslicecoord` is specified in the input file.
- * 
+ *
  * write_silo_slice_step() makes a silo file containing a slice
  * of the scalar field phi and the kinetic energy density. It calls make_kinetic()
  * to populate an array with the kinetic energy density and make_slice() to create
@@ -27,14 +27,14 @@ void make_kinetic(hydro_fields f, hydro_params p, float ***temp) {
   for(x = 1; x <= p.slicex; x++) {
     for(y = 1; y <= p.slicey; y++) {
       for(z = 0; z < p.Lz; z++) {
-	
+
 	temp[x][y][z] = f.kappa[x][y][z]*(f.E[x][y][z]/f.W[x][y][z])
           *(f.W[x][y][z]*f.W[x][y][z]-1.0)*vol;
-        
+
       }
     }
   }
-  
+
   halo_field(temp, p);
 }
 
@@ -45,7 +45,7 @@ void make_curl(hydro_fields f, hydro_params p, float ****temp){
   float loc_tot_enth = 0;
   float mean_enth = 0;
   float ***Wnb = make_field(p);
-  
+
   for(x = 1; x <= p.slicex; x++) {
     for(y = 1; y <= p.slicey; y++) {
       for(z = 0; z < p.Lz; z++) {
@@ -72,7 +72,7 @@ void make_curl(hydro_fields f, hydro_params p, float ****temp){
     }
   }
   halo_field(Wnb, p);
-  
+
   for(x = 1; x <= p.slicex; x++) {
     for(y = 1; y <= p.slicey; y++) {
       for(z = 0; z < p.Lz; z++) {
@@ -82,7 +82,7 @@ void make_curl(hydro_fields f, hydro_params p, float ****temp){
 			    + f.Z[1][x][y][(z-1+p.Lz)%p.Lz]
 			    /Wnb[x][y][(z-1+p.Lz)%p.Lz]
 			    )/(2*p.dx*mean_enth);
-	
+
 	temp[1][x][y][z] = (f.Z[0][x][y][(z+1)%p.Lz]/Wnb[x][y][(z+1)%p.Lz]
 			    - f.Z[0][x][y][(z-1+p.Lz)%p.Lz]
 			    /Wnb[x][y][(z-1+p.Lz)%p.Lz]
@@ -94,7 +94,7 @@ void make_curl(hydro_fields f, hydro_params p, float ****temp){
 			    - f.Z[1][x-1][y][z]/Wnb[x-1][y][z]
 			    - f.Z[0][x][y+1][z]/Wnb[x][y+1][z]
 			    + f.Z[0][x][y-1][z]/Wnb[x][y-1][z]
-			    )/(2*p.dx*mean_enth);	
+			    )/(2*p.dx*mean_enth);
       }
     }
   }
@@ -107,7 +107,7 @@ void make_div(hydro_fields f, hydro_params p, float ***temp){
   float loc_tot_enth = 0;
   float mean_enth = 0;
   float ***Wnb = make_field(p);
-  
+
   for(x = 1; x <= p.slicex; x++) {
     for(y = 1; y <= p.slicey; y++) {
       for(z = 0; z < p.Lz; z++) {
@@ -134,7 +134,7 @@ void make_div(hydro_fields f, hydro_params p, float ***temp){
     }
   }
   halo_field(Wnb, p);
-    
+
   for(x = 1; x <= p.slicex; x++) {
     for(y = 1; y <= p.slicey; y++) {
       for(z = 0; z < p.Lz; z++) {
@@ -151,7 +151,7 @@ void make_div(hydro_fields f, hydro_params p, float ***temp){
   }
 }
 
-  
+
 
 /** Populate an array with velocity magnitude.
  *
@@ -164,15 +164,15 @@ void make_vel(hydro_fields f, hydro_params p, float ***temp) {
   for(x = 1; x <= p.slicex; x++) {
     for(y = 1; y <= p.slicey; y++) {
       for(z = 0; z < p.Lz; z++) {
-   
+
    temp[x][y][z] = sqrt(f.V[0][x][y][z]*f.V[0][x][y][z]
                 + f.V[1][x][y][z]*f.V[1][x][y][z]
                 + f.V[2][x][y][z]*f.V[2][x][y][z]);
-        
+
       }
     }
   }
-  
+
   halo_field(temp, p);
 }
 
@@ -187,15 +187,15 @@ void make_Z(hydro_fields f, hydro_params p, float ***temp) {
   for(x = 1; x <= p.slicex; x++) {
     for(y = 1; y <= p.slicey; y++) {
       for(z = 0; z < p.Lz; z++) {
-   
+
    temp[x][y][z] = sqrt(f.Z[0][x][y][z]*f.Z[0][x][y][z]
                 + f.Z[1][x][y][z]*f.Z[1][x][y][z]
                 + f.Z[2][x][y][z]*f.Z[2][x][y][z]);
-        
+
       }
     }
   }
-  
+
   halo_field(temp, p);
 }
 
@@ -204,36 +204,36 @@ void make_Z(hydro_fields f, hydro_params p, float ***temp) {
 
 /** Populate an array with a scalar quantity in a slice through `temp`.
  *
- * This function takes a 3 dimensional array `temp` 
+ * This function takes a 3 dimensional array `temp`
  * which contains a quanitity defined over the simulation box
- * and populates the 1 dimensional array `slice` with a slice through 
+ * and populates the 1 dimensional array `slice` with a slice through
  * `x=p.siloslicecoord` where `p.siloslicecoord` is specified in the input file.
- * 
- * Only processors containing the coordinate `x=p.siloslicecoord`
- * will participate in this. 
  *
- * First the 1 dimensional array `trim` is populated with the data on 
+ * Only processors containing the coordinate `x=p.siloslicecoord`
+ * will participate in this.
+ *
+ * First the 1 dimensional array `trim` is populated with the data on
  * the local slice. The size of trim is `p.slicey*p.Lz`, and it increases
  * first in `z` and then in `y`.
  *
- * Then the array slice is populated on the rank 0 processor 
- * with `trim` from the other processors. 
- * The size of `slice` is `p.Lz*p.Ly` and it increases first in `z` 
+ * Then the array slice is populated on the rank 0 processor
+ * with `trim` from the other processors.
+ * The size of `slice` is `p.Lz*p.Ly` and it increases first in `z`
  * and then in `y`.
- * 
+ *
  *
  */
 
 void make_slice(hydro_fields f, hydro_params p, float *slice, float ***temp)
 {
 
-  
+
   int x, y, z;
 
   x=p.siloslicecoord;
-  
+
   float *trim = (float *)malloc(p.slicey*p.Lz*sizeof(float));
-  
+
   if(x/p.slicex == p.myposx){
     int localx=x%p.slicex+1;
     for(y=1; y<=p.slicey; y++) {
@@ -315,7 +315,7 @@ void make_slice(hydro_fields f, hydro_params p, float *slice, float ***temp)
 void write_silo_slice_step(hydro_fields f, hydro_params p, int step)
 {
 
-  
+
   int x, y, z, i;
 
 
@@ -330,7 +330,7 @@ void write_silo_slice_step(hydro_fields f, hydro_params p, int step)
 
     /* Create a unique filename for the new Silo file */
     char filename[600];
-    sprintf(filename, "%s/slice-%06d.silo", p.silodir, step);  
+    sprintf(filename, "%s/slice-%06d.silo", p.silodir, step);
     fprintf(stderr,"Writing SLICE to %s\n",filename);
 
     dbfile = DBCreate(filename, DB_CLOBBER, DB_LOCAL,
@@ -378,7 +378,7 @@ void write_silo_slice_step(hydro_fields f, hydro_params p, int step)
   float *slice = (float *)malloc(p.Ly*p.Lz*sizeof(float));
 
 #ifndef SCALAR
-  
+
   make_kinetic(f, p, temp);
 
   make_slice(f, p, slice, temp);
@@ -386,7 +386,7 @@ void write_silo_slice_step(hydro_fields f, hydro_params p, int step)
   if(!p.rank){
     fprintf(stderr,"Slice coord x=%d\n",p.siloslicecoord);
   }
-  
+
   //// write kinetic slice ///
 
 
@@ -398,12 +398,12 @@ void write_silo_slice_step(hydro_fields f, hydro_params p, int step)
 
 
   //// prepare fluid velocity ////
-  
+
   make_vel(f, p, temp);
 
   make_slice(f, p, slice, temp);
 
-  
+
   //// write fluid velocity slice ///
 
 
@@ -415,7 +415,7 @@ void write_silo_slice_step(hydro_fields f, hydro_params p, int step)
 
   //// prepare Z magnitude slice ////
 
-  
+
   make_Z(f, p, temp);
 
   make_slice(f, p, slice, temp);
@@ -441,12 +441,11 @@ void write_silo_slice_step(hydro_fields f, hydro_params p, int step)
          NULL, 0, DB_FLOAT, DB_NODECENT, dboptlist);
   }
 
-  
+
   free_field(p, temp);
 
   //// Write all vel components slice:
-  
-  /*
+
   // Vx slice
   make_slice(f, p, slice, f.V[0]);
   if(!p.rank) {
@@ -490,7 +489,6 @@ void write_silo_slice_step(hydro_fields f, hydro_params p, int step)
     DBPutQuadvar1(dbfile, "Zz", "quadmesh", slice, meshsize, 2,
 		  NULL, 0, DB_FLOAT, DB_NODECENT, dboptlist);
   }
-  */
 
   //// Write all relativistic vorticity (b) components slice:
 
@@ -519,7 +517,7 @@ void write_silo_slice_step(hydro_fields f, hydro_params p, int step)
   }
 
   free_vector(p, temp_vec);
-  
+
   //// make T slice ///
 
   make_slice(f, p, slice, f.T);
@@ -532,7 +530,7 @@ void write_silo_slice_step(hydro_fields f, hydro_params p, int step)
 		  NULL, 0, DB_FLOAT, DB_NODECENT, dboptlist);
   }
 
-  
+
   //// make E slice ///
 
   make_slice(f, p, slice, f.E);
@@ -545,10 +543,10 @@ void write_silo_slice_step(hydro_fields f, hydro_params p, int step)
 		  NULL, 0, DB_FLOAT, DB_NODECENT, dboptlist);
   }
 
-  
-  
+
+
 #endif // !SCALAR
-  
+
   //// make phi slice ///
 
   make_slice(f, p, slice, f.phi);
@@ -561,9 +559,9 @@ void write_silo_slice_step(hydro_fields f, hydro_params p, int step)
 		  NULL, 0, DB_FLOAT, DB_NODECENT, dboptlist);
   }
 
-  
+
   free(slice);
- 
+
 
 
   if(!p.rank) {
@@ -577,9 +575,8 @@ void write_silo_slice_step(hydro_fields f, hydro_params p, int step)
 
 
 
-}  
-  
+}
+
 
 
 #endif // SILO
-
