4b9c3038515a04118162378143a7e4c939cf4b51
Daniel Cutting <d.c.f.cutting@gmail.com>
2021-05-10 16:43:13 +0300

    Rework of global outputs.
    We now write energy quantities as sums seperated into symmetric and broken phase
    components. We compute most energy outputs altogether in the calculate_energies
    function. Slightly reworked which energy components we output. No longer write
    out the total energy - instead we write out all the other separated energies and
    the total energy of the system can be calculated from these.
    
    We also split the sum over pressure into a sum over the symmetric and
    sum over the broken phase. Added two outputs corresponding to a sum over the
    temperature at all sites in symmetric/broken phase.
    
    We now write the sum over the three velocity squared: V^2, rather than sum over
    sqrt(V^2). We split this quantity into symmetric and broken phase sums as well.
    
    Reordered outputs to make them cleaner. Now that the outputs have csv format,
    reordering and adding inputs hopefully won't break scripts in future.

--- energy.c
+++ energy.c
@@ -5,6 +5,217 @@
 #include "hydro.h"
 
 
+
+/** Calculates sum of various components of the total energy, split into
+ *  symmetric and broken phase. Returns as an array of floats.
+ *  
+ *  Sets energies array to be as follows in order:
+ *  rest energy, fluid kinetic energy, kinetic energy scalar, gradient energy scalar, potential
+ *  energy scalar.
+ *  Each energy component is a pair of floats, split into total in symmetric and
+ *  broken phase, in that order. 
+ *  Note that this function does not sum over all sites.
+ */
+void calculate_energies(hydro_fields f, hydro_params p, float *energies) {
+
+  float rest_E_symm = 0;
+  float rest_E_broken = 0;
+  float kin_fluid_symm = 0;
+  float kin_fluid_broken = 0;
+  float kin_phi_symm = 0;
+  float kin_phi_broken = 0;
+  float grad_phi_symm = 0;
+  float grad_phi_broken = 0;
+  float pot_phi_symm = 0;
+  float pot_phi_broken = 0;
+
+  float vol = p.dx*p.dx*p.dx;
+
+  float phi_broken;
+  
+  int x, y, z;
+
+#ifdef BAG
+  phi_broken = p.phi_0;
+#endif
+  
+  for(x = 1; x <= p.slicex; x++) {
+    for(y = 1; y <= p.slicey; y++) {
+      for(z = 0; z < p.Lz; z++) {
+
+#ifndef BAG
+	phi_broken =  (p.alpha*f.T[x][y][z]
+		       + sqrt((p.alpha*p.Tconst)*(p.alpha*f.T[x][y][z])
+			      - 4.0*p.lambda*p.gamma
+			      *(f.T[x][y][z]*f.T[x][y][z] - p.T0*p.T0))
+		       )/(2.0*p.lambda);
+#endif
+
+	if (f.phi[x][y][z] < phi_broken/2){
+#ifndef SCALAR
+	  rest_E_symm += (f.E[x][y][z]/f.W[x][y][z])*vol;
+
+	  kin_fluid_symm += f.kappa[x][y][z]*(f.E[x][y][z]/f.W[x][y][z])
+	    *(f.W[x][y][z]*f.W[x][y][z]-1.0)*vol;
+	    
+#endif
+
+	  grad_phi_symm += 0.5*((f.phi[x+1][y][z] - f.phi[x][y][z])/p.dx)
+	    *((f.phi[x+1][y][z] - f.phi[x][y][z])/p.dx)*vol;
+	
+	  grad_phi_symm += 0.5*((f.phi[x][y+1][z] - f.phi[x][y][z])/p.dx)
+	    *((f.phi[x][y+1][z] - f.phi[x][y][z])/p.dx)*vol;
+	
+	  grad_phi_symm += 0.5*((f.phi[x][y][(z+1)%p.Lz] 
+			  - f.phi[x][y][z])/p.dx)
+	    *((f.phi[x][y][(z+1)%p.Lz] 
+	       - f.phi[x][y][z])/p.dx)*vol;
+
+	  pot_phi_symm += Vf(p, f.T[x][y][z], f.phi[x][y][z])*vol;
+
+	  kin_phi_symm += 0.5*f.pi_future[x][y][z]*f.pi_future[x][y][z]*vol;
+	}
+	else {
+#ifndef SCALAR
+	  rest_E_broken += (f.E[x][y][z]/f.W[x][y][z])*vol;
+
+	  kin_fluid_broken += f.kappa[x][y][z]*(f.E[x][y][z]/f.W[x][y][z])
+	    *(f.W[x][y][z]*f.W[x][y][z]-1.0)*vol;
+	    
+#endif
+
+	  grad_phi_broken += 0.5*((f.phi[x+1][y][z] - f.phi[x][y][z])/p.dx)
+	    *((f.phi[x+1][y][z] - f.phi[x][y][z])/p.dx)*vol;
+	
+	  grad_phi_broken += 0.5*((f.phi[x][y+1][z] - f.phi[x][y][z])/p.dx)
+	    *((f.phi[x][y+1][z] - f.phi[x][y][z])/p.dx)*vol;
+	
+	  grad_phi_broken += 0.5*((f.phi[x][y][(z+1)%p.Lz] 
+			  - f.phi[x][y][z])/p.dx)
+	    *((f.phi[x][y][(z+1)%p.Lz] 
+	       - f.phi[x][y][z])/p.dx)*vol;
+
+	  pot_phi_broken += Vf(p, f.T[x][y][z], f.phi[x][y][z])*vol;
+
+	  kin_phi_broken += 0.5*f.pi_future[x][y][z]*f.pi_future[x][y][z]*vol;
+	}
+      }
+    }
+  }
+  energies[0] =  rest_E_symm;
+  energies[1] =  rest_E_broken;
+  energies[2] =  kin_fluid_symm;
+  energies[3] =  kin_fluid_broken;
+  energies[4] =  kin_phi_symm;
+  energies[5] =  kin_phi_broken;
+  energies[6] =  grad_phi_symm;
+  energies[7] =  grad_phi_broken;
+  energies[8] =  pot_phi_symm;
+  energies[9] =  pot_phi_broken;  
+}
+
+/** Calculates sum the temperature over sites on the core, split into
+ *  symmetric and broken phase. Returns as an array of floats, with symmetric
+ *  first and broken second.
+ *  
+ *  Note that this function does not sum over all sites, only the ones on the
+ *  current core.
+ */
+void calculate_T_sum(hydro_fields f, hydro_params p, float *T_sum) {
+
+  float T_sum_symm = 0;
+  float T_sum_broken = 0;
+
+  float vol = p.dx*p.dx*p.dx;
+
+  float phi_broken;
+  
+  int x, y, z;
+#ifndef SCALAR
+#ifdef BAG
+  phi_broken = p.phi_0;
+#endif
+  
+  for(x = 1; x <= p.slicex; x++) {
+    for(y = 1; y <= p.slicey; y++) {
+      for(z = 0; z < p.Lz; z++) {
+
+#ifndef BAG
+	phi_broken =  (p.alpha*f.T[x][y][z]
+		       + sqrt((p.alpha*p.Tconst)*(p.alpha*f.T[x][y][z])
+			      - 4.0*p.lambda*p.gamma
+			      *(f.T[x][y][z]*f.T[x][y][z] - p.T0*p.T0))
+		       )/(2.0*p.lambda);
+#endif
+
+	if (f.phi[x][y][z] < phi_broken/2){
+	  T_sum_symm += f.T[x][y][z];
+
+	}
+	else {
+	  T_sum_broken += f.T[x][y][z];
+	}
+      }
+    }
+  }
+#endif // !SCALAR
+  T_sum[0] =  T_sum_symm;
+  T_sum[1] =  T_sum_broken;
+  
+}
+
+/** Compute the total pressure. Split this into symmetric and broken phases,
+ * symmetric first.
+ *
+ * NB: This function does _not_ currently sum over all sites.
+ */
+void calculate_pressure_sum(hydro_fields f, hydro_params p, float *pressure_sum) {
+
+  int x, y, z;
+
+  float vol;
+
+  float press_symm = 0;
+  float press_broken = 0;
+  float phi_broken;
+  
+  vol = p.dx*p.dx*p.dx;
+#ifndef SCALAR
+#ifdef BAG
+  phi_broken = p.phi_0;
+#endif
+  
+  for(x = 1; x <= p.slicex; x++) {
+    for(y = 1; y <= p.slicey; y++) {
+      for(z = 0; z < p.Lz; z++) {
+
+#ifndef BAG
+	phi_broken =  (p.alpha*f.T[x][y][z]
+		       + sqrt((p.alpha*p.Tconst)*(p.alpha*f.T[x][y][z])
+			      - 4.0*p.lambda*p.gamma
+			      *(f.T[x][y][z]*f.T[x][y][z] - p.T0*p.T0))
+		       )/(2.0*p.lambda);
+#endif
+
+	if (f.phi[x][y][z] < phi_broken/2){
+	  press_symm += f.p[x][y][z]*vol;
+
+	}
+	else {
+	  press_broken += f.p[x][y][z]*vol;
+	}
+      }
+    }
+  }
+#endif // !SCALAR
+
+  pressure_sum[0] = press_symm;
+  pressure_sum[1] = press_broken;
+
+}
+
+
+
 /** Computes the total energy in the scalar field.
  *
  * Separately computes the kinetic, gradient and potential energy of
@@ -271,41 +482,6 @@ float rest_energy(hydro_fields f, hydro_params p) {
 
 
 
-/** Compute the total pressure (despite the name).
- *
- * NB: This function does _not_ currently sum over all sites.
- */
-float avg_pressure(hydro_fields f, hydro_params p) {
-
-  int x, y, z;
-
-  float vol;
-
-  float press;
-
-  vol = p.dx*p.dx*p.dx;
-
-  press = 0.0;
-
-  for(x = 1; x <= p.slicex; x++) {
-    for(y = 1; y <= p.slicey; y++) {
-      for(z = 0; z < p.Lz; z++) {
-
-#ifndef SCALAR
-	// kinetic energy
-	press += f.p[x][y][z]*vol;
-#endif // SCALAR
-
-      }
-      
-    }
-  }
-
-  return press;
-
-
-
-}
    
 			    
 
