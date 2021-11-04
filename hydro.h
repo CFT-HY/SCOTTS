12a3faf1d49a03907f607ca59a669655a9c1daed
Daniel Cutting <d.c.f.cutting@gmail.com>
2021-11-04 17:28:43 +0200

    Modified transport.c so that the higher order advection scheme takes other flux limiters than just Van Leer. Followed the advection notes in doc folder.

--- hydro.h
+++ hydro.h
@@ -679,9 +679,9 @@ void advect_E(hydro_fields f, hydro_params p, int adv_order);
 void advect_Z(hydro_fields f, hydro_params p, int adv_order);
 void donor_E_dir(hydro_fields f, hydro_params p, int dir);
 void donor_Z_dir(hydro_fields f, hydro_params p, int dir);
-void van_leer_E(hydro_fields f, hydro_params p, int dir);
-void van_leer_Z(hydro_fields f, hydro_params p, int dir);
-
+void transport_E(hydro_fields f, hydro_params p, int dir);
+void transport_Z(hydro_fields f, hydro_params p, int dir);
+static inline float flux_limiter(float r);
 
 
 // initial.c
